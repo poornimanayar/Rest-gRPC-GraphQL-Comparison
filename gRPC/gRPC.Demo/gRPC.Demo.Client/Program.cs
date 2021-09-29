0cfb69fd76672ecd346baeebebc08bfd22362c42
Poornima Nayar <poornimakrishnav@gmail.com>
2021-09-29 12:28:33 +0100

    gRPC client added

---
+++ gRPC/gRPC.Demo/gRPC.Demo.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using gRPC.Demo.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPC.Demo.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //create a channel, a connection to the service
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            //instantiate the client
+            var client = new User.UserClient(channel);
+
+            //call the get users stub method
+            var reply = await client.GetUsersAsync(new UserListRequestModel());
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Users");
+            Console.WriteLine("========================================================");
+
+            foreach(var user in reply.Users)
+            {
+                Console.WriteLine($"{user.Id} . {user.Name} - {user.Headline}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
